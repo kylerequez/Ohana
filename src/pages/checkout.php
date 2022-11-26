6d24431a25e451ff63ace8b85b2e8acc24866992
LopezJohn07 <jemueljj@gmail.com>
2022-11-27 01:36:43 +0800

    local

--- src/pages/checkout.php
+++ src/pages/checkout.php
@@ -15,78 +15,68 @@
     <link rel="stylesheet" href="/Ohana/src/css/pawcart.css">
     <link rel="stylesheet" href="/Ohana/src/css/navbar.css">
 
-
     <?php include_once 'stylesheets.php'; ?>
     <style>
+        #head {
+            margin-top:10%;
+        }
         @media screen and (min-width: 360px) and (max-width: 929.98px) {}
     </style>
 </head>
 
 <body style="background-color: #FAF8F0;">
     <main>
-        <!-- REGISTERED USERS NAVIGATION BAR-->
         <?php include_once 'Rnavbar.php'; ?>
-
         <div class="container-fluid">
-            <!-- FULL WIDTH OF THE PAGE - BOOTSTRAP COMPONENT-->
-
-            <section class="carthead" style="margin-top:5%; ">
+            <section class="carthead" id="head">
                 <div class="cartheader">
                     <img src="/Ohana/src/images/Pages/checkoutheader.png" class="img-fluid" width="100%">
                 </div>
             </section>
 
             <div class="container">
-                <section class="paymentsection" style="margin-top:5%;">
-
+                <section class="paymentsection">
                     <div class="paymentmethod" id="paymentmethod">
                         <div class="d-flex justify-content-center mb-5">
-                            <div class="card text-center mx-3" style="width: 18rem;">
-                                <div class="card-body">
-                                    <img src="/Ohana/src/images/payment/pay1.png" width="150px" height="150px">
-                                    <h5 class="card-title"> GCASH</h5>
-                                    <p class="card-text"> No. of users who chose this: 5</p>
-                                    <a href="#" class="btn mt-3" style="background-color:#db6551; color:white;"> Select </a>
-                                </div>
-                            </div>
-
-                            <div class="card text-center mx-3" style="width: 18rem;">
-                                <div class="card-body">
-                                    <img src="/Ohana/src/images/payment/pay2.png" width="150px" height="150px">
-                                    <h5 class="card-title"> BANK TRANSFER </h5>
-                                    <p class="card-text"> No. of users who chose this: 10</p>
-                                    <a href="#" class="btn mt-3" style="background-color:#db6551; color:white;"> Select</a>
-                                </div>
-                            </div>
-
-                            <div class="card text-center mx-3" style="width: 18rem;">
-                                <div class="card-body">
-                                    <img src="/Ohana/src/images/payment/pay3.png" width="150px" height="150px">
-                                    <h5 class="card-title"> CASH </h5>
-                                    <p class="card-text"> No. of users who chose this: 15</p>
-                                    <a href="#" class="btn mt-3" style="background-color:#db6551; color:white;"> Select</a>
+                            <div class="card text-center mx-3" style="width: 50rem;">
+                                <div class="d-flex">
+                                    <div class="card-body">
+                                        <img src="/Ohana/src/images/payment/pay1.png" width="150px" height="150px">
+                                        <p class="card-text"> No. of users who chose this: 5</p>
+                                        <input class="form-check-input mt-3" type="radio" name="inlineRadioOptions" id="gcash" value="gcash" />
+                                        <label class="form-check-label mt-3 mb-3" for="payment-method"> GCASH </label>
+                                    </div>
+                                    <div class="card-body">
+                                        <img src="/Ohana/src/images/payment/pay2.png" width="150px" height="150px">
+                                        <p class="card-text"> No. of users who chose this: 10</p>
+                                        <input class="form-check-input mt-3" type="radio" name="inlineRadioOptions" id="bank" value="bank" />
+                                        <label class="form-check-label mt-3 mb-3" for="payment-method"> BANK TRANSFER </label>
+                                    </div>
+                                    <div class="card-body">
+                                        <img src="/Ohana/src/images/payment/pay3.png" width="150px" height="150px">
+                                        <p class="card-text"> No. of users who chose this: 15</p>
+                                        <input class="form-check-input mt-3" type="radio" name="inlineRadioOptions" id="cash" value="cash" />
+                                        <label class="form-check-label mt-3 mb-3" for="payment-method"> CASH </label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="paymentselected">
-                        <h1 class="text-center mt-3 mb-5 fs-5"> YOU HAVE SELECTED: *PAYMENT METHOD* </h1>
-                    </div>
-
-                </section>
-                <section class="paymentnote">
-                    <center>
-                        <div class="card rounded-3 mb-4" style="width:75%;">
-                            <p class="fs-6 mt-5"> Note: TRANSACTIONS WILL BE DONE FACE TO FACE, YOU ONLY NEED TO SELECT A PAYMENT METHOD OF YOUR CHOICE </p>
-                            <p class="fs-6 mt-3 mb-5"> Friendly Reminder: Kindly Read the <b>Terms and Conditions for Cancellation and Payment Policy.</b> </p>
-                        </div>
-                        <a href="/upload-proof"><button type="button" name="btn-Payment" class="btn btn-block btn-lg" style="background-color:#c0b65a; color:white; width:300px;">
-                                Proceed </button></a> <br><br>
-                        <a href="/home"><button type="button" class="btn btn-outline-dark btn-lg" style="width:300px;"> Back to Home</button></a>
-                    </center>
                 </section>
             </div>
+          
+            <section class="paymentnote mb-5">
+                <center>
+                    <div class="card rounded-3 mb-5" style="width:75%;">
+                        <p class="fs-6 mt-5"> Note: TRANSACTIONS WILL BE DONE FACE TO FACE, YOU ONLY NEED TO SELECT A PAYMENT METHOD OF YOUR CHOICE </p>
+                        <p class="fs-6 mt-3 mb-5"> Friendly Reminder: Kindly Read the <b>Terms and Conditions for Cancellation and Payment Policy.</b> </p>
+                    </div>
+                    <a href="/upload-proof"><button type="button" name="btn-Payment" class="btn btn-block btn-lg" style="background-color:#c0b65a; color:white; width:300px;">
+                            Proceed </button></a> <br><br>
+                    <a href="/home"><button type="button" class="btn btn-outline-dark btn-lg" style="width:300px;"> Back to Home</button></a>
+                </center>
+            </section>
+        </div>
 
         </div>
     </main>
