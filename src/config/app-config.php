a8114fdfafdb7a4374581ee5c71ddb33f8cfb0a0
kylerequez <kyle.requez.code@gmail.com>
2022-12-10 17:07:43 +0800

    For pushing

--- src/config/app-config.php
+++ src/config/app-config.php
@@ -1,6 +1,6 @@
 <?php
-define('DOMAIN_NAME', 'localhost');
-// define('DOMAIN_NAME', 'ohanawebapp.000webhostapp.com');
+// define('DOMAIN_NAME', 'localhost');
+define('DOMAIN_NAME', 'ohanawebapp.000webhostapp.com');
 
 define('EMAIL_USERNAME', 'ohana.kennel.business@gmail.com');
 define('EMAIL_KEY', 'ctdlqnibzafgmwyj');
--- src/config/db-config.php
+++ src/config/db-config.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$database = "Ohana_DB";
-$username = "root";
-$password = "";
-
 // $servername = "localhost";
-// $database = "id19881316_ohanadb";
-// $username = "id19881316_root";
-// $password = "Ohanakennelph@2022";
+// $database = "Ohana_DB";
+// $username = "root";
+// $password = "";
+
+$servername = "localhost";
+$database = "id19881316_ohanadb";
+$username = "id19881316_root";
+$password = "Ohanakennelph@2022";
