31bdcfa78e0448cff2f570eebe7b328e40eb8473
kylerequez <kyle.requez.code@gmail.com>
2022-12-21 13:13:14 +0800

    For pushing

--- src/api/account/userforgotpassword.php
+++ src/api/account/userforgotpassword.php
@@ -15,7 +15,23 @@ $controller = new AccountController($services, $logservices);
 if ($_SERVER["REQUEST_METHOD"] == "GET" and !empty($_GET["email"])) {
     $controller->forgotPasswordRequest($_SERVER["REQUEST_METHOD"], $_GET["email"]);
 } else if ($_SERVER["REQUEST_METHOD"] == "POST" and !empty($_SESSION["email"])) {
+    if (!isset($_SESSION['time']) || $_SESSION['time'] + (5 * 60) < time()) {
+        $_SESSION['msg'] = "You have exceeded the maximum time period (5 minutes) to enter a new password. Please send another change password request again.";
+        unset($_SESSION["email"]);
+        unset($_SESSION["userOtp"]);
+        unset($_SESSION["token"]);
+        header("Location: https://" . DOMAIN_NAME . "/forgot-password");
+        exit();
+    }
     $controller->forgotPasswordRequest($_SERVER["REQUEST_METHOD"], $_SESSION["email"]);
 } else if ($_SERVER["REQUEST_METHOD"] == "GET" and (!empty($_SESSION["email"] and !empty($token)))) {
+    if (!isset($_SESSION['time']) || $_SESSION['time'] + (5 * 60) < time()) {
+        $_SESSION['msg'] = "You have exceeded the maximum time period (5 minutes) to click the link. Please send another change password request again.";
+        unset($_SESSION["email"]);
+        unset($_SESSION["userOtp"]);
+        unset($_SESSION["token"]);
+        header("Location: https://" . DOMAIN_NAME . "/forgot-password");
+        exit();
+    }
     $controller->resendForgotPasswordRequest($_SERVER["REQUEST_METHOD"], $_SESSION["email"], $token);
 }
